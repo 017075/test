92f4908a89fb84d91b7bdc5f84507273a343fd2a
young <18234007040@163.com>
2017-12-20 14:56:16 +0800

    登录拦截实现(请求拦截实现)

---
+++ src/main/java/helloWorld/util/MyInterceptor.java
@@ -0,0 +1,44 @@
+package helloWorld.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by zhaojianhong on 2017/12/20.
+ */
+public class MyInterceptor implements HandlerInterceptor {
+    Logger logger = Logger.getLogger(MyInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // TODO Auto-generated method stub
+        logger.info(">>>MyInterceptor>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
+        //获取session
+        HttpSession session = request.getSession(true);
+        //判断用户ID是否存在，不存在就跳转到登录界面
+        if(session.getAttribute("userId") == null){
+            logger.info("------:跳转到login页面！");
+            response.sendRedirect("/test");
+            return false;
+        }else{
+            session.setAttribute("userId", session.getAttribute("userId"));
+            return true;
+        }
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        logger.info(">>>MyInterceptor1>>>>>>>请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        logger.info(">>>MyInterceptor1>>>>>>>在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
+    }
+}
