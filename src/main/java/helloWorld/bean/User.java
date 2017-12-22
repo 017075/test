317f3b7d1fd3c49011582c5b8871ed42411926ab
young <18234007040@163.com>
2017-12-22 16:04:47 +0800

    MGB添加

---
+++ src/main/java/helloWorld/bean/User.java
@@ -0,0 +1,75 @@
+package helloWorld.bean;
+
+import java.util.Date;
+
+public class User {
+    private Long userId;
+
+    private String userName;
+
+    private String password;
+
+    private Date createdDate;
+
+    private Date modifiedDate;
+
+    private Integer phoneNumber;
+
+    private Date birthday;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
